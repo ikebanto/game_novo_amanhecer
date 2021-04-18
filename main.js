9e08e3f1c9ebfddb4887cf15cccf2bdcb7748c5b
ikebanto <ikebanto@gmail.com>
2021-04-18 14:14:26 -0300

    Adição no main.js e style.css

--- main.js
+++ main.js
@@ -14,6 +14,19 @@ const imgs = [
   'turismo_sexual.svg'
 ];
 
+const msg_asscf = ['Título','Mensagem para abuso sexual sem contato físico'];
+const msg_assef = ['Título','Mensagem para abuso extra familiar'];
+const msg_asi = ['Título','Mensagem para abuso em instituições'];
+const msg_asif = ['Título','Mensagem para abuso intra familiar'];
+const msg_asccf = ['Título','Mensagem para abuso sexual com contato físico'];
+const msg_d100 = ['Título','Mensagem para disk 100'];
+const msg_pi = ['Título','Mensagem para pornografia infantil'];
+const msg_tsag = ['Título','Mensagem para trabalho sexual agenciado'];
+const msg_tsau = ['Título','Mensagem para trabalho sexual autônomo'];
+const msg_trafsex = ['Título','Mensagem para tráfico sexual'];
+const msg_trocsex = ['Título','Mensagem para trocas sexual'];
+const msg_turisex = ['Título','Mensagem para turismo sexual'];
+
 let cardHTML = "";
 
 imgs.forEach(img => {
@@ -65,9 +78,82 @@ function resetCards(isMatch = false) {
   if (isMatch) {
     firstCard.removeEventListener("click", flipCard);
     secondCard.removeEventListener("click", flipCard);
+	exibeMensagem(secondCard);
   }
 
   [firstCard, secondCard, lockCards] = [null, null, false];
+}
+
+function selecionaMensagem(card){
+
+	switch (card.dataset.card) {
+		case "abuso_sexual_contato_fisico.svg":
+			return msg_asscf;
+			break;
+		case "abuso_sexual_extra_familiar.svg":
+			return msg_assef;
+			break;
+		case "abuso_sexual_inst.svg":
+			return msg_asi;
+			break;
+		case "abuso_sexual_intra_familiar.svg":
+			return msg_asif;
+			break;
+	    case "contato_fisico.svg":
+			return msg_asccf;
+			break;
+		case "disk100.svg":
+			return msg_d100;
+			break;
+		case "pornografia_infantil.svg":
+			return msg_pi;
+			break;
+	    case "trabalho_sexual_agenciado.svg":
+			return msg_tsag;
+			break;
+		case "trabalho_sexual_autonomo.svg":
+			return msg_tsau;
+			break;
+		case "trafico_sexual.svg":
+			return msg_trafsex;
+			break;
+		case "trocas_sexuais.svg":
+			return msg_trocsex;
+			break;
+		case "turismo_sexual.svg":
+			return msg_turisex;
+	}
+}
+
+(function shuffle (){
+	cards.forEach ( card => {
+		let rand = Math.floor(Math.random()*12);
+		card.style.order = rand;
+	})
+})();
+
+function exibeMensagem (card){
+
+	var img = null;
+	var titulo = selecionaMensagem(card)[0];
+	var mensagem = selecionaMensagem(card)[1];
+
+	const modal = document.getElementById("modal");
+	const divMsg = document.getElementById("divMsg");
+	const h2 = document.getElementById("titulo");
+	const p = document.getElementById("mensagem");
+
+	modal.style.display = "block";
+	divMsg.style.display = "block";
+	h2.innerHTML = "";
+	p.innerHTML = "";
+
+	h2.innerHTML = titulo;
+	p.innerHTML = mensagem;
+}
+
+function fechaMensagem (){
+	const modal = document.getElementById("modal").style.display = 'none';
 }
 
 cards.forEach(card => card.addEventListener("click", flipCard));
